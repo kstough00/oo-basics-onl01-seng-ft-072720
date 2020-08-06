806100cefff1a89ee2b7ad8a0e5115673a2c8e7c
Kyle Stough <kstough00@gmail.com>
2020-08-06 18:53:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-class Shoe# Make your shoe class here!
+class Book
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
